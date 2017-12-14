d15ef1fc34a55cf088160b235b586f9704f6161e
laurkim <laurakeem@gmail.com>
2017-12-14 10:22:26 -0500

    set up serializer and cors

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DifferentiaBackEnd
+module Synthesis
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -24,6 +24,15 @@ module DifferentiaBackEnd
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+       allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+     end
+
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
