da6a274662d03db0919626b153cdb807ff7d2cfe
laurkim <laurakeem@gmail.com>
2017-12-15 17:00:46 -0500

    remove user tokens from front end

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :access_token, :refresh_token, :spotify_url, :profile_img_url, :href, :uri, :full_library, :last_library_update
+  attributes :id, :username, :spotify_url, :profile_img_url, :full_library
 end
