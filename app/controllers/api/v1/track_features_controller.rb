5d9d8f162ee06fa6c7cac4c3db76a5d3f3edab33
laurkim <laurakeem@gmail.com>
2017-12-19 19:53:04 -0500

    gets track features

--- app/controllers/api/v1/track_features_controller.rb
+++ app/controllers/api/v1/track_features_controller.rb
@@ -1,26 +1,10 @@
 class Api::V1::TrackFeaturesController < ApplicationController
   def create
-    # debugger
-    # content = params[:q]
-    # username = ENV[USERNAME]
-    # password = ENV["PASSWORD"]
-    #
-    # response = Excon.post("https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13"
-    #   body: content,
-    #   headers: { "Content-Type": "text/plain" },
-    #   user: username,
-    #   password: password,
-    #   query: {
-    #      "raw_scores": true,
-    #      "consumption_preferences": true,
-    #      "version": "2017-10-13"
-    #   }
-    # )
-    #
-    # insights = JSON.parse(response.body)
-    # new_insight = PersonalityInsight.create(insight: insights, user_id: my_user.id)
-    # my_user.personality_insights << new_insight
-    #
-    # render json: { personality_insights: insights }
+    body = {track_ids: params[:q].chomp('"').reverse.chomp('"').reverse}
+    decoded = JWT.decode(my_user.access_token, ENV["MY_SECRET"], ENV["ES"])
+    header = {'Authorization': "Bearer " + decoded[0]["access_token"]}
+    track_features_response = RestClient.get("https://api.spotify.com/v1/audio-features/?ids=#{body[:track_ids]}", header)
+    track_features_params = JSON.parse(track_features_response.body)
+    render json: {track_features: track_features_params}
   end
 end
